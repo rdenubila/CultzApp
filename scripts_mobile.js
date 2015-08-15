3b84c98d966001f388a86506022e26699f215db0
rdenubila <rdenubila@gmail.com>
2015-08-15 19:36:50 -0300

    teste fb

--- scripts_mobile.js
+++ scripts_mobile.js
@@ -10,12 +10,36 @@ function ready(){
 }
 
 
+function statusChangeCallback(response) {
+	if (response.status === 'connected') {
+		getUserData();
+	} else if (response.status === 'not_authorized') {
+		loginFB();
+		$("#loading").fadeOut("fast");
+	} else {
+		loginFB();
+		$("#loading").fadeOut("fast");
+	}
+}
+
+
 function checkLoginState() {
 	
 	$("#loading").fadeIn("fast");
 
 	facebookConnectPlugin.getLoginStatus( 
+		function(response) {
+			statusChangeCallback(response);
+		},
+        function (response) { alert(JSON.stringify(response)) }
+    );
+
+}
+
+
+function loginFB(){
+	
+	facebookConnectPlugin.login( ["email", "user_friends"],
         function (response) { alert(JSON.stringify(response)) },
         function (response) { alert(JSON.stringify(response)) });
-
 }
