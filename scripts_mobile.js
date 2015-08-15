0b68520102a483fa64e31a5ee2f361e9dfd550cc
rdenubila <rdenubila@gmail.com>
2015-08-15 19:20:42 -0300

    test fb

--- scripts_mobile.js
+++ scripts_mobile.js
@@ -8,3 +8,14 @@ function ready(){
 	checkInit();
 
 }
+
+
+function checkLoginState() {
+	
+	$("#loading").fadeIn("fast");
+
+	facebookConnectPlugin.getLoginStatus( 
+        function (response) { alert(JSON.stringify(response)) },
+        function (response) { alert(JSON.stringify(response)) });
+
+}
